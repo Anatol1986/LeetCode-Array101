34296622d4327ae160005fee8072f698cfe15b92
Anatoly Uarmolovich <yarmolovich1986@gmail.com>
2021-04-30 16:11:03 +0300

    Squares of a Sorted Array In progress

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -42,39 +42,62 @@ The length of input array is a positive integer and will not exceed 10,000
 Given an array nums of integers, return how many of them contain an even number of digits.
 
 */
+//
+//let testArray = [555,901,482,1771]
+//let secondTestArray = [12,345,2,6,7896]
+//let thirdTestArray = [252]
+//
+//func findNumbersWithEvenNumberOfDigits(_ nums: [Int]) -> Int {
+//
+//    if nums.count == 0 {
+//        return 0
+//    }
+//
+//    var testArray = [Int]()
+//
+//    for i in 0..<nums.count {
+//
+//        var num = nums[i]
+//        var count = 0
+//        while num != 0 {
+//            let digit = abs(num % 10)
+//            if digit >= 0 {
+//                count += 1
+//            }
+//            num = num / 10
+//        }
+//
+//        if count % 2 == 0 {
+//            print("Count: \(count)")
+//            testArray.append(nums[i])
+//        }
+//    }
+//    return testArray.count
+//}
+//
+//findNumbersWithEvenNumberOfDigits(testArray)
+//findNumbersWithEvenNumberOfDigits(secondTestArray)
+//findNumbersWithEvenNumberOfDigits(thirdTestArray)
 
-let testArray = [555,901,482,1771]
-let secondTestArray = [12,345,2,6,7896]
-let thirdTestArray = [252]
+/*
+ Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in
+ non-decreasing order.
+*/
 
-func findNumbersWithEvenNumberOfDigits(_ nums: [Int]) -> Int {
-
+let firstTestArray = [-4,-1,0,3,10]
+let secondTestArray = [-7,-3,2,3,11]
+func sortedSquares(_ nums: [Int]) -> [Int] {
     if nums.count == 0 {
-        return 0
+        return nums
     }
 
-    var testArray = [Int]()
+    var temporaryArray = [Int]()
 
     for i in 0..<nums.count {
-
-        var num = nums[i]
-        var count = 0
-        while num != 0 {
-            let digit = abs(num % 10)
-            if digit >= 0 {
-                count += 1
-            }
-            num = num / 10
-        }
-
-        if count % 2 == 0 {
-            print("Count: \(count)")
-            testArray.append(nums[i])
-        }
+        let value = abs(nums[i] * nums[i])
+        temporaryArray.append(value)
     }
-    return testArray.count
-}
 
-findNumbersWithEvenNumberOfDigits(testArray)
-findNumbersWithEvenNumberOfDigits(secondTestArray)
-findNumbersWithEvenNumberOfDigits(thirdTestArray)
+    
+
+}
