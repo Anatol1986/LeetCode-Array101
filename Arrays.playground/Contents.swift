fa96c893bef6cf2210762cdec31814d64e7f1f88
Anatoly Uarmolovich <yarmolovich1986@gmail.com>
2021-04-30 14:21:27 +0300

    Find Numbers with Even Number of Digits

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -4,45 +4,77 @@ import UIKit
 
 Given a binary array, find the maximum number of consecutive 1s in this array.
 
-Example 1:
-Input: [1,1,0,1,1,1]
-Output: 3
-Explanation: The first two digits or the last three digits are consecutive 1s.
-    The maximum number of consecutive 1s is 3.
-Note:
-
 The input array will only contain 0 and 1.
 The length of input array is a positive integer and will not exceed 10,000
  */
 
-func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
-    
-    var tempArray = [Int]()
-    
-    var tempCounter = 0
-    
-    if nums.count == 1 && nums[0] == 1 {
-        return 1
+//func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+//
+//    var tempArray = [Int]()
+//
+//    var tempCounter = 0
+//
+//    if nums.count == 1 && nums[0] == 1 {
+//        return 1
+//    }
+//
+//    for i in 0..<nums.count {
+//        if nums[i] == 1 {
+//            tempCounter += 1
+//        } else {
+//            tempArray.append(tempCounter)
+//            tempCounter = 0
+//        }
+//    }
+//
+//    for y in 0..<tempArray.count {
+//        if tempCounter < tempArray[y] {
+//            tempCounter = tempArray[y]
+//        } else {
+//            continue
+//        }
+//    }
+//
+//    return tempCounter
+//}
+
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+*/
+
+let testArray = [555,901,482,1771]
+let secondTestArray = [12,345,2,6,7896]
+let thirdTestArray = [252]
+
+func findNumbersWithEvenNumberOfDigits(_ nums: [Int]) -> Int {
+
+    if nums.count == 0 {
+        return 0
     }
-    
+
+    var testArray = [Int]()
+
     for i in 0..<nums.count {
-        if nums[i] == 1 {
-            tempCounter += 1
-        } else {
-            tempArray.append(tempCounter)
-            tempCounter = 0
+
+        var num = nums[i]
+        var count = 0
+        while num != 0 {
+            let digit = abs(num % 10)
+            if digit >= 0 {
+                count += 1
+            }
+            num = num / 10
+        }
+
+        if count % 2 == 0 {
+            print("Count: \(count)")
+            testArray.append(nums[i])
         }
     }
-    
-    for y in 0..<tempArray.count {
-        if tempCounter < tempArray[y] {
-            tempCounter = tempArray[y]
-        } else {
-            continue
-        }
-    }
-    
-    return tempCounter
+    return testArray.count
 }
 
-
+findNumbersWithEvenNumberOfDigits(testArray)
+findNumbersWithEvenNumberOfDigits(secondTestArray)
+findNumbersWithEvenNumberOfDigits(thirdTestArray)
